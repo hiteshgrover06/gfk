6635acf4986c2bf10d90b2bf9d97c038dffed17f
hitesh <hiteshgrover06@gmail.com>
2018-03-25 18:35:25 +0200

    changed to let

--- lineChart.js
+++ lineChart.js
@@ -2,10 +2,10 @@
 var myApp = myApp || {};
 (function () {
     myApp.onFileInputChange = () => {
-        var reader = new FileReader();
+        let reader = new FileReader();
         reader.onload = function () {
-            var data = reader.result.split(";").join(" ").split("\n");
-            var result = {};
+            let data = reader.result.split(";").join(" ").split("\n");
+            let result = {};
             for (let i = 1; i < data.length - 1; i++) {
                 let date = data[i].split(" ")[0];
                 let isYes = data[i].split(" ")[2].trim() == "yes";
@@ -21,16 +21,16 @@ var myApp = myApp || {};
             // Fromatted object
             document.getElementById('out').innerText = JSON.stringify(result);
 
-            var xAxisData = [];
-            var yAxisData = [];
+            let xAxisData = [];
+            let yAxisData = [];
             Object.keys(result).forEach(function (key) {
                 let yesPercentage = (result[key].y / result[key].t) * 100;
                 xAxisData.push(key);
                 yAxisData.push(yesPercentage);
             });
 
-            var ctx = document.getElementById('canvas').getContext('2d');
-            var myChart = new Chart(ctx, {
+            let ctx = document.getElementById('canvas').getContext('2d');
+            let myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: xAxisData,
